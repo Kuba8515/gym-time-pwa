542009a079aa50143793fa67b79c6963a970d447
Jakub <jakub.gazdziak@gmail.com>
2021-11-28 15:35:17 +0100

    Add heroku

--- util/database.ts
+++ util/database.ts
@@ -1,6 +1,9 @@
 import camelcaseKeys from 'camelcase-keys';
 import dotenvSafe from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
+
+setPostgresDefaultsOnHeroku();
 
 export type User = {
   id: number;
@@ -13,6 +16,7 @@ export type Workout = {
   id: number;
   title: string;
   description: string;
+  userId: number;
 };
 
 export type Exercise = {
@@ -24,6 +28,18 @@ export type Exercise = {
   sets: string;
   reps: string;
   smallImg: string;
+};
+
+export type UserWorkout = {
+  userWorkoutId: number;
+  userId: number;
+  workoutId: number;
+};
+
+export type ExerciseWorkout = {
+  exerciseWorkout: number;
+  workoutId: number;
+  exerciseId: number;
 };
 
 export type UserWithPasswordHash = User & {
@@ -46,17 +62,21 @@ declare module globalThis {
 
 function connectOneTimeToDatabase() {
   let sql;
-  // Heroku setup
-  if (process.env.NODE_ENV === 'production') {
+
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    // Heroku needs SSL connections but
+
+    // has an "unauthorized" certificate
+
+    // https://devcenter.heroku.com/changelog-items/852
+
     sql = postgres({ ssl: { rejectUnauthorized: false } });
   } else {
-    // In development, connect only once to database !
     if (!globalThis.postgresSqlClient) {
       globalThis.postgresSqlClient = postgres();
     }
     sql = globalThis.postgresSqlClient;
   }
-
   return sql;
 }
 
@@ -195,17 +215,11 @@ export async function deleteUserById(id: number) {
 export async function getWorkoutByUserId(userId: number) {
   const workouts = await sql<[Workout]>`
     SELECT
-      workouts.id,
-      workouts.title,
-      workouts.description
+      *
     FROM
-      users,
-      users_workouts,
       workouts
     WHERE
-      users.id = ${userId} AND
-      users_workouts.user_id = users.id AND
-      users_workouts.workout_id = workouts.id;
+      user_id = ${userId}
   `;
   return workouts.map((workout) => camelcaseKeys(workout));
 }
@@ -334,6 +348,62 @@ export async function getWorkout(id: number) {
   return camelcaseKeys(workout[0]);
 }
 
+export async function getWorkoutbyWorkoutId(workoutId: number) {
+  if (!workoutId) return undefined;
+  const [workouts] = await sql<[Workout]>`
+    SELECT
+      *
+    FROM
+      workouts
+    WHERE
+      id = ${workoutId};
+  `;
+
+  return camelcaseKeys(workouts);
+}
+
+export async function deleteWorkoutbyId(id: number) {
+  const workouts = await sql`
+  DELETE FROM
+    workouts
+  WHERE
+    id = ${id}
+  RETURNING
+    *;
+`;
+  return camelcaseKeys(workouts[0]);
+}
+
+export async function updateWorkoutById(
+  id: number,
+  {
+    title,
+    description,
+    imageUrl,
+  }: {
+    title: string;
+    description: string;
+    imageUrl: string;
+  },
+) {
+  const workouts = await sql`
+    UPDATE
+      workouts
+    SET
+      title = ${title},
+      description = ${description},
+      image_url = ${imageUrl}
+    WHERE
+      id = ${id}
+    RETURNING
+      id,
+      title,
+      description,
+      image_url
+  `;
+  return camelcaseKeys(workouts[0]);
+}
+
 export async function getExercisebyWorkout(workoutId: number) {
   const exercises = await sql<[Exercise]>`
     SELECT
@@ -353,4 +423,27 @@ export async function getExercisebyWorkout(workoutId: number) {
       exercises.id = exercises_workouts.exercise_id;
   `;
   return exercises.map((exercise) => camelcaseKeys(exercise));
+}
+
+export async function insertToUserWorkout({
+  userId,
+  workoutId,
+  userWorkoutId,
+}: {
+  userId: number;
+  workoutId: number;
+  userWorkoutId: number;
+}) {
+  const [userWorkout] = await sql<[UserWorkout]>`
+      INSERT INTO wantlist
+        (user_id, workout_id, workout_user_id)
+      VALUES
+        (${userId}, ${workoutId}, ${userWorkoutId})
+      RETURNING
+        id,
+        user_id,
+        workout_id,
+        workout_user_id;
+    `;
+  return camelcaseKeys(userWorkout);
 }
