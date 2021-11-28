c7b248bd24fb1e8d39088b175c85b447f8a27073
Jakub <jakub.gazdziak@gmail.com>
2021-11-28 12:31:52 +0100

    Fix heroku

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import WorkoutCard from '../components/WorkoutCard';
 
-export default function Home(props) {
+export default function HomePage(props) {
   return (
     <Layout username={props.username}>
       <Head>
@@ -19,6 +19,8 @@ export default function Home(props) {
           </a>
         </Link>
       </div>
+      {/* {props.userWorkout.length === 0 ? (
+        <> */}
       <div className="border-2 border-gray-300 mt-6" />
       <div className="md:mb-0 mt-2 text-center max-w-md mx-auto">
         <ul className="m-2 align-center p-4 align-center ml-16 mr-16 mb-12 gap-8 list-none">
@@ -36,16 +38,27 @@ export default function Home(props) {
           })}
         </ul>
       </div>
+      {/* </>
+      ) : (
+        <div>{props.userWorkout.find(userId)}</div>
+        <span>hello world</span>
+      )}  */}
     </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
-  const { getWorkouts } = await import('../util/database');
+  const { getWorkouts, getWorkoutByUserId, getUserBySessionToken } =
+    await import('../util/database');
+  // const allowedUser = await getUserBySessionToken(context.req.cookies.token);
   const workouts = await getWorkouts(context.query.id);
+  // const userWorkout = await getWorkoutByUserId(session.userId);
+  // const userId = session?.userId;
   return {
     props: {
       workouts,
+      // userWorkout,
+      // userId,
     },
   };
 }
--- util/database.ts
+++ util/database.ts
@@ -13,6 +13,7 @@ export type Workout = {
   id: number;
   title: string;
   description: string;
+  userId: number;
 };
 
 export type Exercise = {
@@ -24,6 +25,18 @@ export type Exercise = {
   sets: string;
   reps: string;
   smallImg: string;
+};
+
+export type UserWorkout = {
+  userWorkoutId: number;
+  userId: number;
+  workoutId: number;
+};
+
+export type ExerciseWorkout = {
+  exerciseWorkout: number;
+  workoutId: number;
+  exerciseId: number;
 };
 
 export type UserWithPasswordHash = User & {
@@ -195,17 +208,11 @@ export async function deleteUserById(id: number) {
 export async function getWorkoutByUserId(userId: number) {
   const workouts = await sql<[Workout]>`
     SELECT
-      workouts.id,
-      workouts.title,
-      workouts.description
+      *
     FROM
-      users,
-      users_workouts,
       workouts
     WHERE
-      users.id = ${userId} AND
-      users_workouts.user_id = users.id AND
-      users_workouts.workout_id = workouts.id;
+      user_id = ${userId}
   `;
   return workouts.map((workout) => camelcaseKeys(workout));
 }
@@ -334,6 +341,62 @@ export async function getWorkout(id: number) {
   return camelcaseKeys(workout[0]);
 }
 
+export async function getWorkoutbyWorkoutId(workoutId: number) {
+  if (!workoutId) return undefined;
+  const [workouts] = await sql<[Workout]>`
+    SELECT
+      *
+    FROM
+      workouts
+    WHERE
+      id = ${workoutId};
+  `;
+
+  return camelcaseKeys(workouts);
+}
+
+export async function deleteWorkoutbyId(id: number) {
+  const workouts = await sql`
+  DELETE FROM
+    workouts
+  WHERE
+    id = ${id}
+  RETURNING
+    *;
+`;
+  return camelcaseKeys(workouts[0]);
+}
+
+export async function updateWorkoutById(
+  id: number,
+  {
+    title,
+    description,
+    imageUrl,
+  }: {
+    title: string;
+    description: string;
+    imageUrl: string;
+  },
+) {
+  const workouts = await sql`
+    UPDATE
+      workouts
+    SET
+      title = ${title},
+      description = ${description},
+      image_url = ${imageUrl}
+    WHERE
+      id = ${id}
+    RETURNING
+      id,
+      title,
+      description,
+      image_url
+  `;
+  return camelcaseKeys(workouts[0]);
+}
+
 export async function getExercisebyWorkout(workoutId: number) {
   const exercises = await sql<[Exercise]>`
     SELECT
@@ -353,4 +416,27 @@ export async function getExercisebyWorkout(workoutId: number) {
       exercises.id = exercises_workouts.exercise_id;
   `;
   return exercises.map((exercise) => camelcaseKeys(exercise));
+}
+
+export async function insertToUserWorkout({
+  userId,
+  workoutId,
+  userWorkoutId,
+}: {
+  userId: number;
+  workoutId: number;
+  userWorkoutId: number;
+}) {
+  const [userWorkout] = await sql<[UserWorkout]>`
+      INSERT INTO wantlist
+        (user_id, workout_id, workout_user_id)
+      VALUES
+        (${userId}, ${workoutId}, ${userWorkoutId})
+      RETURNING
+        id,
+        user_id,
+        workout_id,
+        workout_user_id;
+    `;
+  return camelcaseKeys(userWorkout);
 }
